9421e33f64df811a4c5b6741e92d617820fd179c
Leonel <Leonel.Ruiz112@gmail.com>
2020-09-16 00:01:54 -0300

    animación del título

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+/*
+    let background1 = document.getElementById('background1')
+
+    window.addEventListener('scroll', (e) => {
+       let last_known_scroll_position = window.scrollY
+       let bgpy = background1.style.backgroundPositionY
+
+       background1.style.backgroundPositionY = 
+       console.log('ultima posicion: '+ last_known_scroll_position)
+       console.log(bgpy)
+    })*/
+})
