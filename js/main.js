2753afc3b3de341967d8a5efb533f992ad78a017
Leonel <Leonel.Ruiz112@gmail.com>
2020-11-25 01:49:34 -0300

    cambios en colores

--- js/main.js
+++ js/main.js
@@ -11,17 +11,7 @@ $(document).ready(function(){
         }
     })
 
-    /*
-    let background1 = document.getElementById('background1')
-
-    window.addEventListener('scroll', (e) => {
-       let last_known_scroll_position = window.scrollY
-       let bgpy = background1.style.backgroundPositionY
-
-       background1.style.backgroundPositionY = 
-       console.log('ultima posicion: '+ last_known_scroll_position)
-       console.log(bgpy)
-    })*/
+    
 
     $(window).resize( function(e){
         let height = window.innerWidth
